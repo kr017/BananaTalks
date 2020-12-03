0b857b87bd00ab0899a0ad0d701bc7249daed6ca
Kiran <krmate634@gmail.com>
2020-12-03 10:21:19 +0530

    handler for button click to

--- app.js
+++ app.js
@@ -5,10 +5,22 @@
 // alert("Hi "+name);
 
 var btnTranslate = document.querySelector("#btn-translate");
+var inputTextarea= document.querySelector("#input_textarea");
+var outputDiv=document.querySelector("#outputDiv");
 
-btnTranslate.addEventListener("click",handleTranslateButtonClick);
+btnTranslate.addEventListener("click",handleInputTextareaChange);
 
-function handleTranslateButtonClick() {
+// function handleTranslateButtonClick() {
     
-    alert("Hello Moto")
+//     alert("Hello Moto")
+// }
+
+// inputTextarea.addEventListener("change",handleInputTextareaChange)
+
+
+function handleInputTextareaChange() {
+    alert("contents changed to "+inputTextarea.value);
 }
+
+
+outputDiv.innerHTML="Kiran"
